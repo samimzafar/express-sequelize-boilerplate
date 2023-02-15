18e91d28922740a5ed713b4eb6d6eaa7e2d4ad8a
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-02-15 11:30:24 +0500

    removed extra routers

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,10 @@ const { Router } = require("express");
 const router = Router();
 
 // Routers
-const productRouter = require("./product");
-const orderRouter = require("./order");
-const studentRouter = require("./student");
-const bankRouter = require("./bank");
 const devRouter = require("./dev");
 const projectRouter = require("./project");
 
-router.use("/products", productRouter);
-router.use("/orders", orderRouter);
-router.use("/banks", bankRouter);
-router.use("/students", studentRouter);
+//Routes
 router.use("/devs", devRouter);
 router.use("/projects", projectRouter);
 
