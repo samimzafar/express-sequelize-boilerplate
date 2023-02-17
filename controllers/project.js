30036ffec8388156e1ec64799a7f77e3f068f4c7
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-02-17 11:41:47 +0500

    changes project code

--- controllers/project.js
+++ controllers/project.js
@@ -1,24 +1,91 @@
-const { Projects } = require("../models");
+const { Projects, DevProjects, Devs } = require("../models");
 module.exports = {
- create: async (req, res) => {
-  try {
-   const { query: { name } } = req;
-   let projectSaved = await Projects.create({
-    name
-   });
-   return res.send({
-    status: 200,
-    success: true,
-    message: "Project added successfully.",
-    data: { projectSaved },
-   });
-  } catch (err) {
-   console.log(err.message);
-   return res
-    .status(err.status || 500)
-    .send({
-     error: err.message || "Something went wrong!"
-    });
-  }
- },
+  create: async (req, res) => {
+    try {
+      const {
+        query: { name },
+      } = req;
+      let projectSaved = await Projects.create({
+        name,
+      });
+      return res.send({
+        status: 200,
+        success: true,
+        message: "Project added successfully.",
+        data: projectSaved,
+      });
+    } catch (err) {
+      console.log(err.message);
+      return res.status(err.status || 500).send({
+        error: err.message || "Something went wrong!",
+      });
+    }
+  },
+
+  getAll: async (req, res) => {
+    try {
+      let projectSaved = await Projects.findAll({
+        attributes: ["id", "name"],
+        include: [
+          {
+            model: DevProjects,
+            as: "devProjects",
+            attributes: ["id"],
+            include: {
+              model: Devs,
+              as: "devs",
+              attributes: ["id", "name"],
+            },
+          },
+        ],
+      });
+      return res.send({
+        status: 200,
+        success: true,
+        message: "Project added successfully.",
+        data: projectSaved,
+      });
+    } catch (err) {
+      console.log(err.message);
+      return res.status(err.status || 500).send({
+        error: err.message || "Something went wrong!",
+      });
+    }
+  },
+
+  getOne: async (req, res) => {
+    try {
+      const {
+        params: { name },
+      } = req;
+      console.log(name);
+      let projectSaved = await Projects.findAll({
+        where: { name },
+        attributes: ["name"],
+        include: [
+          {
+            model: DevProjects,
+            as: "devProjects",
+            attributes: ["id"],
+            include: {
+              model: Devs,
+              as: "devs",
+              attributes: ["name"],
+            },
+          },
+        ],
+      });
+      return res.send({
+        status: 200,
+        success: true,
+        message: "Project Fetched successfully.",
+        data: projectSaved,
+      });
+    } catch (err) {
+      console.log(err.message);
+      return res.status(err.status || 500).send({
+        error: err.message || "Something went wrong!",
+      });
+    }
+  },
 };
