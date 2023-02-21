2bb58a6864a2e2fcb06adb5c0eb340a14ea7b933
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-02-21 16:20:44 +0500

    apiError class added

--- utils/apiError.js
+++ utils/apiError.js
@@ -1,13 +1,12 @@
 class ApiError extends Error {
- constructor(status, message) {
-   super();
-   Object.setPrototypeOf(this, new.target.prototype);
-   this.status = status;
-   this.success = false;
-   this.message = message;
-
-   Error.captureStackTrace(this);
- }
+  constructor(status, message) {
+    super();
+    Object.setPrototypeOf(this, new.target.prototype);
+    this.status = status;
+    this.success = false;
+    this.message = message;
+    Error.captureStackTrace(this);
+  }
 }
 
 module.exports = ApiError;
