1b19ff806003daa384b8854d58d573fc801e210d
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-02-22 12:20:38 +0500

    user created op

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,24 @@
+const { sequelize, Users } = require("../models");
+const ApiError = require("../utils/apiError");
 const builtResponse = require("../utils/builtResponse");
 module.exports = {
  create: async (req, res, next) => {
+  const transaction = await sequelize.transaction();
   try {
    const role = req.header("role");
    const { name, phone_number, email } = req.body;
-   let data = { name, phone_number, email, role };
-   builtResponse(res, "data added", data);
+   let user = await Users.findOne({
+    where: { email }
+   });
+   if (user) {
+    throw new ApiError(409, "User Already Exists");
+   } else {
+    user = await Users.create({ name, phone_number, email, role });
+    builtResponse(res, "User Created successfully", user);
+    await transaction.commit();
+   }
   } catch (err) {
+   await transaction.rollback();
    next(err);
   }
  }
